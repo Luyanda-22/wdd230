cfab34c9ae671088269ac1d40a280c1521446638
Luyanda-22 <luyandamascot@gmail.com>
2024-01-27 22:00:50 +0200

    changed lesson 02 to lesson02

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,17 @@
+//Getting the current year
+const currentYear = new Date().getFullYear();
+
+//Updating the content of the element with id "copyright"
+document.getElementById("copyright").textContent = currentYear;
+
+//Getting the last modified timestamp of the document
+const lastModifiedTimestamp = document.lastModified
+
+//Formatting the timestamp as a date
+const lastModifiedDate = new Date(lastModifiedTimestamp);
+
+//Formattting the date as a string
+const formattedDate = lastModifiedDate.toLocaleString();
+
+//Updatting the content of the element wit id "lastModified"
+document.getElementById("lastModified").textContent = formattedDate;
