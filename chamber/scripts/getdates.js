e60ff579cd4c3a0157f1be2f07d1c3c06e3db909
Luyanda-22 <luyandamascot@gmail.com>
2024-02-05 03:05:05 +0200

    chamber site

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,29 @@
+//Getting the current year
+const currentYear = new Date().getFullYear();
+
+//Updating the content of the element with id "copyright"
+document.getElementById("copyright").textContent = currentYear;
+
+//Getting the last modified timestamp of the document
+const lastModifiedTimestamp = document.lastModified
+
+//Formatting the timestamp as a date
+const lastModifiedDate = new Date(lastModifiedTimestamp);
+
+//Formattting the date as a string
+const formattedDate = lastModifiedDate.toLocaleString();
+
+//Updatting the content of the element wit id "lastModified"
+document.getElementById("lastModified").textContent = formattedDate;
+
+//Hamburger menu
+
+const hamburger = document.querySelector('#menu');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+	nav.classList.toggle('open');
+	hamburger.classList.toggle('open');
+
+       
+});
