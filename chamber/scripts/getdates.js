5d307e42ccc3359e48bcf97f58a9d3c332b9b9e1
Luyanda-22 <luyandamascot@gmail.com>
2024-03-16 18:47:44 +0200

    made some changes

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -90,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function() {
     localStorage.setItem("visitCount", visitCountValue.toString());
 });
 
-//Form/////////////////////////////////////////////////////
+
 
 
 
